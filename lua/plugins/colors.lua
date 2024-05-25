5a6a14e8d4409c87dcfeaf55eac87c4dcb2439a6
kutiny <alex08lta@gmail.com>
2024-05-25 02:15:50 -0300

    chore: add config for catppuccin

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -2,7 +2,6 @@ local theme = "rose-pine"
 local theme_file = "~/.nvim_theme"
 
 function Color(color)
-
     if color then
         vim.fn.system("echo -n " .. color .. " > " .. theme_file)
     else
@@ -19,8 +18,7 @@ function Color(color)
 end
 
 vim.keymap.set('n', '<leader>t', function()
-    local t= vim.fn.input("Theme > ");
-
+    local t=vim.fn.input('Theme > ');
     Color(t)
 end)
 
@@ -28,7 +26,56 @@ return {
     {
         'catppuccin/nvim',
         name = "catppuccin",
-        priority = 1000
+        priority = 1000,
+        config = function()
+            require('catppuccin').setup({
+                flavour = "auto", -- latte, frappe, macchiato, mocha
+                background = { -- :h background
+                    light = "latte",
+                    dark = "mocha",
+                },
+                transparent_background = true, -- disables setting the background color.
+                show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+                term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+                dim_inactive = {
+                    enabled = false, -- dims the background color of inactive window
+                    shade = "dark",
+                    percentage = 0.15, -- percentage of the shade to apply to the inactive window
+                },
+                no_italic = false, -- Force no italic
+                no_bold = false, -- Force no bold
+                no_underline = false, -- Force no underline
+                styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+                comments = { "italic" }, -- Change the style of comments
+                conditionals = { "italic" },
+                loops = {},
+                functions = {},
+                keywords = {},
+                strings = {},
+                variables = {},
+                numbers = {},
+                booleans = {},
+                properties = {},
+                types = {},
+                operators = {},
+                -- miscs = {}, -- Uncomment to turn off hard-coded styles
+                },
+                color_overrides = {},
+                custom_highlights = {},
+                default_integrations = true,
+                integrations = {
+                    cmp = true,
+                    gitsigns = true,
+                    nvimtree = true,
+                    treesitter = true,
+                    notify = false,
+                    mini = {
+                        enabled = true,
+                        indentscope_color = "",
+                    },
+                },
+            })
+        end
     },
     {
         'dracula/vim',
