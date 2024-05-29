2f708a3d576ae76ca72d67ef3243769d43d0f1e7
kutiny <alex08lta@gmail.com>
2024-05-29 18:54:25 -0300

    chore: update config

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -2,5 +2,17 @@ return {
    "m4xshen/hardtime.nvim",
    event = 'VeryLazy',
    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-   opts = {}
+   opts = {},
+   config = function ()
+       require("hardtime").setup({
+            max_count = 5,
+       })
+   end,
+   init = function ()
+       vim.keymap.set(
+       "n",
+       "<Leader>h",
+       ":Hardtime toggle<CR>"
+       )
+   end
 }
